0e9b88b53d14df20ca52918f06ced61c80155f97
Nick Casadei <nickcasadei@icloud.com>
2025-05-25 18:47:42 +0100

    title metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
-  title: "Wild flowers",
+  title: "Wild flowers Community",
   description: "Wild flowers community",
   icons: {
     icon: "/White_flower.svg",
