2b5a8960e95566be7ed94d6281aec41b9c405af1
Nick Casadei <nickcasadei@icloud.com>
2025-06-11 22:28:26 +0100

    credentials

--- src/lib/googleSheet.js
+++ src/lib/googleSheet.js
@@ -1,11 +1,9 @@
 import { google } from "googleapis";
 
 export async function getSheetData() {
+  const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
   const auth = new google.auth.GoogleAuth({
-    credentials: {
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    },
+    credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
